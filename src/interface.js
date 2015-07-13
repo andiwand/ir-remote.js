c86f3809678908185dec5992255018c0fe8d3799
andiwand <stefl.andreas@gmail.com>
2015-07-14 00:08:13 +0200

    apply commons namespace

--- src/interface.js
+++ src/interface.js
@@ -3,6 +3,6 @@ var IR = IR || {};
 IR.interface = IR.interface || {};
 
 $.extend(IR.interface, {
-  load: Util.notImplemented,       // TODO
-  save: Util.notImplemented        // TODO
+  load: andiwand.notImplemented,       // TODO: doc
+  save: andiwand.notImplemented        // TODO: doc
 });
